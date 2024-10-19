657e9c8480ab6f7985ff4713bcd16a63bd35c215
TT <43578259+Lonexera@users.noreply.github.com>
2024-10-19 13:57:38 +0300

    update project to kts

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.android.application").version("8.3.0") apply(false)
+    id("org.jetbrains.kotlin.android").version("1.9.20") apply(false)
+    id("com.google.dagger.hilt.android").version("2.52") apply(false)
+    id("com.google.gms.google-services").version("4.4.2") apply(false)
+}
+
+subprojects {
+//    apply(plugin = "org.jetbrains.dokka")
+}
