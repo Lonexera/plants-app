0b4ba6ce16a4f9685d81f32575d66531e1978a34
TT <43578259+Lonexera@users.noreply.github.com>
2024-10-19 16:38:31 +0300

    move :app to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.google.services)
     id("kotlin-kapt")
-    id("dagger.hilt.android.plugin")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -34,57 +34,45 @@ android {
 }
 
 dependencies {
-
-    //implementation 'androidx.core:core-ktx:1.6.0'
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    implementation("androidx.fragment:fragment-ktx:1.8.4")
-
-    // Glide
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-
-    // Room
-    val room = "2.6.1"
-    implementation("androidx.room:room-runtime:$room")
-    implementation("androidx.room:room-ktx:$room")
-    kapt("androidx.room:room-compiler:$room")
-
-    // Timber
-    implementation("com.jakewharton.timber:timber:5.0.1")
-
-    // viewBinding delegate
-    implementation("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.4.7")
-
-    // JUnit 4 framework
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.assertj:assertj-core:3.18.1")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.1")
-
-    // Hilt
-    val hilt = "2.52"
-    implementation("com.google.dagger:hilt-android:$hilt")
-    kapt("com.google.dagger:hilt-android-compiler:$hilt")
-
-    // WorkManager
-    implementation("androidx.work:work-runtime-ktx:2.7.0")
-    implementation("androidx.hilt:hilt-work:1.0.0")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
-
-    // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:29.0.0"))
-    implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.4.1")
-    implementation("com.google.firebase:firebase-storage")
-    implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.android.gms:play-services-auth:19.2.0")
-
     // Statistics Contract
     implementation(project(":plantStatsContract"))
     // Domain Module
     implementation(project(":domain"))
 
+    // UI
+    implementation(libs.androidx.appcompat)
+    implementation(libs.android.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.fragment.ktx)
+    // viewBinding delegate
+    implementation(libs.kirich1409.viewbinding.delegate)
+    // Glide
+    implementation(libs.bumptech.glide)
     // Shimmering
-    implementation("com.facebook.shimmer:shimmer:0.5.0")
+    implementation(libs.facebook.shimmer)
+    // Hilt
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.compiler)
+    // Timber
+    implementation(libs.jakewharton.timber)
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
+    // WorkManager
+    implementation(libs.androidx.work.runtime)
+    implementation(libs.androidx.hilt.work)
+    kapt(libs.androidx.hilt.compiler)
+    // Firebase
+    implementation(platform(libs.google.firebase.bom))
+    implementation(libs.google.firebase.firestore)
+    implementation(libs.google.firebase.storage)
+    implementation(libs.google.firebase.auth.ktx)
+    // Play services
+    implementation(libs.kotlinx.coroutines.play.services)
+    implementation(libs.android.gms.play.services.auth)
+    // JUnit 4 framework
+    testImplementation(libs.junit)
+    testImplementation(libs.assertj)
+    testImplementation(libs.test.coroutines)
 }
