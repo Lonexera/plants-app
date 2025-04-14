c097126cf551e15169e4c95b9811a24be47fa140
TT <43578259+Lonexera@users.noreply.github.com>
2025-04-15 00:52:35 +0300

    part 1: migrate Plant Detail screen to compose

--- app/src/main/java/com/example/plantsapp/presentation/ui/plantdetail/PlantDetailFragment.kt
+++ app/src/main/java/com/example/plantsapp/presentation/ui/plantdetail/PlantDetailFragment.kt
@@ -2,10 +2,13 @@ package com.example.plantsapp.presentation.ui.plantdetail
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -16,9 +19,7 @@ import com.example.plantsapp.databinding.FragmentPlantDetailBinding
 import com.example.plantsapp.domain.model.Plant
 import com.example.plantsapp.presentation.ui.loading.LoadingDialog
 import com.example.plantsapp.presentation.ui.loading.hideOnLifecycle
-import com.example.plantsapp.presentation.ui.plantdetail.adapter.DetailTasksAdapter
 import com.example.plantsapp.presentation.ui.plantdetail.adapter.PlantPhotosAdapter
-import com.example.plantsapp.presentation.ui.utils.loadPicture
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -35,7 +36,6 @@ class PlantDetailFragment : Fragment(R.layout.fragment_plant_detail) {
             plantName = requireArguments().plantName
         )
     }
-    private val tasksAdapter = DetailTasksAdapter()
     private val photosAdapter = PlantPhotosAdapter()
     private val loadingDialog by lazy { LoadingDialog(requireContext()) }
 
@@ -44,71 +44,88 @@ class PlantDetailFragment : Fragment(R.layout.fragment_plant_detail) {
         setHasOptionsMenu(true)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                PlantDetailScreen(
+                    viewModel = detailViewModel,
+                    onNavigateBack = {
+                        requireActivity().supportFragmentManager.popBackStack()
+                    }
+                )
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         loadingDialog.hideOnLifecycle(viewLifecycleOwner)
 
-        with(detailViewModel) {
-            appBarTitle.observe(viewLifecycleOwner) {
-                activity?.title = it
-            }
-
-            plantDetailUiState.observe(viewLifecycleOwner) { state ->
-                when (state) {
-                    is PlantDetailViewModel.PlantDetailUiState.InitialState -> {
-                        changeLoadingStates(
-                            isLoadingIndicatorVisible = true,
-                            isLoadingDialogVisible = false
-                        )
-                    }
-                    is PlantDetailViewModel.PlantDetailUiState.DataIsLoaded -> {
-                        changeLoadingStates(
-                            isLoadingIndicatorVisible = false,
-                            isLoadingDialogVisible = false
-                        )
-                        showPlantDetail(state.plant)
-                        tasksAdapter.submitList(state.tasks)
-                    }
-                    is PlantDetailViewModel.PlantDetailUiState.LoadingState -> {
-                        changeLoadingStates(
-                            isLoadingIndicatorVisible = false,
-                            isLoadingDialogVisible = true
-                        )
-                    }
-                    is PlantDetailViewModel.PlantDetailUiState.FinalState -> {
-                        changeLoadingStates(
-                            isLoadingIndicatorVisible = false,
-                            isLoadingDialogVisible = false
-                        )
-                        requireActivity().supportFragmentManager.popBackStack()
-                    }
-                }
-            }
-
-            plantPhotosUiState.observe(viewLifecycleOwner) { state ->
-                when (state) {
-                    is PlantDetailViewModel.PlantPhotosUiState.InitialState -> {
-                        binding.rvPlantPhotos.isVisible = false
-                        binding.layoutPlantGalleryShimmering.root.isVisible = true
-                    }
-                    is PlantDetailViewModel.PlantPhotosUiState.PhotosAreLoaded -> {
-                        photosAdapter.submitList(state.photos)
-                        binding.rvPlantPhotos.isVisible = true
-                        binding.layoutPlantGalleryShimmering.root.isVisible = false
-                    }
-                }
-            }
-        }
+//        with(detailViewModel) {
+//            appBarTitle.observe(viewLifecycleOwner) {
+//                activity?.title = it
+//            }
+//
+//            plantDetailUiState.observe(viewLifecycleOwner) { state ->
+//                when (state) {
+//                    is PlantDetailViewModel.PlantDetailUiState.InitialState -> {
+//                        changeLoadingStates(
+//                            isLoadingIndicatorVisible = true,
+//                            isLoadingDialogVisible = false
+//                        )
+//                    }
+//                    is PlantDetailViewModel.PlantDetailUiState.DataIsLoaded -> {
+//                        changeLoadingStates(
+//                            isLoadingIndicatorVisible = false,
+//                            isLoadingDialogVisible = false
+//                        )
+//                        showPlantDetail(state.plant)
+//                        tasksAdapter.submitList(state.tasks)
+//                    }
+//                    is PlantDetailViewModel.PlantDetailUiState.LoadingState -> {
+//                        changeLoadingStates(
+//                            isLoadingIndicatorVisible = false,
+//                            isLoadingDialogVisible = true
+//                        )
+//                    }
+//                    is PlantDetailViewModel.PlantDetailUiState.FinalState -> {
+//                        changeLoadingStates(
+//                            isLoadingIndicatorVisible = false,
+//                            isLoadingDialogVisible = false
+//                        )
+//                        requireActivity().supportFragmentManager.popBackStack()
+//                    }
+//                }
+//            }
+//
+//            plantPhotosUiState.observe(viewLifecycleOwner) { state ->
+//                when (state) {
+//                    is PlantDetailViewModel.PlantPhotosUiState.InitialState -> {
+//                        binding.rvPlantPhotos.isVisible = false
+//                        binding.layoutPlantGalleryShimmering.root.isVisible = true
+//                    }
+//                    is PlantDetailViewModel.PlantPhotosUiState.PhotosAreLoaded -> {
+//                        photosAdapter.submitList(state.photos)
+//                        binding.rvPlantPhotos.isVisible = true
+//                        binding.layoutPlantGalleryShimmering.root.isVisible = false
+//                    }
+//                }
+//            }
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_plant_detail_appbar, menu)
 
-        detailViewModel.plantDetailUiState.observe(viewLifecycleOwner) { state ->
-            menu.findItem(R.id.action_delete_plant).isVisible =
-                state is PlantDetailViewModel.PlantDetailUiState.DataIsLoaded
-        }
+//        detailViewModel.plantDetailUiState.observe(viewLifecycleOwner) { state ->
+//            menu.findItem(R.id.action_delete_plant).isVisible =
+//                state is PlantDetailViewModel.PlantDetailUiState.DataIsLoaded
+//        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -123,12 +140,6 @@ class PlantDetailFragment : Fragment(R.layout.fragment_plant_detail) {
 
     private fun showPlantDetail(plant: Plant) {
         with(binding) {
-            ivDetailPlant.loadPicture(plant.plantPicture)
-
-            etDetailPlantName.setText(plant.name.value)
-            etDetailSpeciesName.setText(plant.speciesName)
-
-            rvDetailTasks.adapter = tasksAdapter
             rvPlantPhotos.adapter = photosAdapter
         }
     }
---
+++ app/src/main/java/com/example/plantsapp/presentation/ui/plantdetail/PlantDetailScreen.kt
@@ -0,0 +1,240 @@
+package com.example.plantsapp.presentation.ui.plantdetail
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidthIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.placeholder
+import com.example.plantsapp.R
+import com.example.plantsapp.domain.model.Task
+import com.example.plantsapp.presentation.temp.Loading
+import com.example.plantsapp.presentation.ui.utils.getColorRes
+import com.example.plantsapp.presentation.ui.utils.getIconRes
+
+@Composable
+fun PlantDetailScreen(
+    viewModel: PlantDetailViewModel,
+    onNavigateBack: () -> Unit,
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.closeScreen.collect {
+            onNavigateBack()
+        }
+    }
+
+    PlantDetailScreen(
+        uiState = uiState,
+    )
+}
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+private fun PlantDetailScreen(
+    uiState: PlantDetailViewModel.UiState,
+) {
+    when (uiState.detailState) {
+        PlantDetailViewModel.UiState.PlantDetailState.Loading -> Loading()
+        is PlantDetailViewModel.UiState.PlantDetailState.DataIsLoaded -> {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                item {
+                    GlideImage(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .padding(all = 16.dp)
+                            .clip(RoundedCornerShape(10.dp)),
+                        model = uiState.detailState.plant.plantPicture,
+                        contentDescription = "Plant picture",
+                        contentScale = ContentScale.Crop,
+                        failure = placeholder(R.drawable.ic_baseline_image_24),
+                    )
+                }
+
+                item {
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+//                            .padding(horizontal = 16.dp)
+                        ,
+                        value = uiState.detailState.plant.name.value,
+                        onValueChange = {},
+                        readOnly = true,
+                        textStyle = LocalTextStyle.current.copy(fontSize = 20.sp),
+                        label = {
+                            Text(text = stringResource(R.string.hint_detail_plant_name))
+                        },
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.Transparent,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                        )
+                    )
+                }
+
+                item {
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+//                            .padding(horizontal = 16.dp)
+                        ,
+                        value = uiState.detailState.plant.speciesName,
+                        onValueChange = {},
+                        readOnly = true,
+                        textStyle = LocalTextStyle.current.copy(fontSize = 20.sp),
+                        label = {
+                            Text(text = stringResource(R.string.hint_detail_species_name))
+                        },
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = Color.Transparent,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                        )
+                    )
+                }
+
+                item {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        text = stringResource(R.string.title_plant_tasks),
+                        fontSize = 18.sp,
+                        color = colorResource(R.color.dark_grey),
+                    )
+                }
+
+                item {
+                    LazyRow(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        verticalAlignment = Alignment.Top,
+                        contentPadding = PaddingValues(horizontal = 16.dp),
+                    ) {
+                        items(uiState.detailState.tasks) { task ->
+                            TaskItem(task)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun TaskItem(
+    task: Task,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier
+            .width(intrinsicSize = IntrinsicSize.Min)
+            .requiredWidthIn(min = 100.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(bottom = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(56.dp)
+                    .fillMaxWidth()
+                    .background(color = colorResource(id = task.getColorRes())),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = task.getIconRes()),
+                    contentDescription = "Task icon",
+                    tint = colorResource(R.color.white)
+                )
+            }
+
+            Text(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                text = stringResource(id = task.getShortTitleRes()),
+                fontSize = 14.sp,
+                color = colorResource(R.color.dark_grey),
+                textAlign = TextAlign.Center,
+            )
+
+            Text(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                text = task.getTaskDetailText(),
+                fontSize = 14.sp,
+                color = colorResource(R.color.dark_grey),
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+@StringRes private fun Task.getShortTitleRes():  Int {
+    return when (this) {
+        is Task.WateringTask -> R.string.title_plant_detail_watering_task
+        is Task.SprayingTask -> R.string.title_plant_detail_spraying_task
+        is Task.LooseningTask -> R.string.title_plant_detail_loosening_task
+        is Task.TakingPhotoTask -> R.string.title_plant_detail_taking_photo_task
+    }
+}
+
+@Composable
+private fun Task.getTaskDetailText(): String {
+    return stringResource(
+        id = R.string.title_task_detail,
+        pluralStringResource(
+            id = R.plurals.msg_creation_frequency_units,
+            count = frequency,
+            frequency
+        )
+    )
+}
