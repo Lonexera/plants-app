0f1475fdfea14e8f757bbc0d4ae6cd2210d4c5dc
TT <43578259+Lonexera@users.noreply.github.com>
2025-04-14 22:45:53 +0300

    migrate Profile screen to compose

--- app/src/main/java/com/example/plantsapp/presentation/ui/authentication/AuthFragment.kt
+++ app/src/main/java/com/example/plantsapp/presentation/ui/authentication/AuthFragment.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
-import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.plantsapp.R
-import com.example.plantsapp.databinding.FragmentAuthBinding
-import com.example.plantsapp.presentation.ui.loading.LoadingDialog
 import com.example.plantsapp.presentation.ui.tasksfordays.TasksForDaysFragment
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,7 +17,6 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class AuthFragment : Fragment(R.layout.fragment_auth) {
-    private val binding: FragmentAuthBinding by viewBinding(FragmentAuthBinding::bind)
     private val viewModel: AuthViewModel by viewModels()
 
     @Inject
