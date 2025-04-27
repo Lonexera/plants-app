0005a8c302213297e5c679f218ec08ba845decdb
TT <43578259+Lonexera@users.noreply.github.com>
2025-04-27 09:19:34 +0300

    update detekt + apply to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.hilt) apply(false)
     alias(libs.plugins.google.services) apply(false)
     alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.detekt)
 }
 
 subprojects {
